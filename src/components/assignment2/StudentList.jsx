8b502a44999f0f032efa24f2275846ec1e2e8c84
Soham <sohamkarandikar007@gmail.com>
2025-12-02 23:33:39 +0530

    Update StudentList component

--- src/components/assignment2/StudentList.jsx
+++ src/components/assignment2/StudentList.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Student from './Student';
 
 const students = [
-    { id: 1, name: 'Soham', age: 21, course: 'Computer Science' },
-    { id: 2, name: 'Rahul', age: 22, course: 'Mechanical Engineering' },
-    { id: 3, name: 'Priya', age: 20, course: 'Electronics' },
+    { id: 1, name: 'Soham', age: 19, course: 'Computer Science' },
+    { id: 2, name: 'Rayan', age: 19, course: 'Mechanical Engineering' },
+    { id: 3, name: 'Piyush', age: 20, course: 'Electronics' },
 ];
 
 export default function StudentList() {
